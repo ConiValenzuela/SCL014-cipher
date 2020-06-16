170db09c0b9d361fb5e607d3d4a72c11f89d6a8f
Coni <constanzavalenzuelat@gmail.com>
2020-06-16 11:58:36 -0400

    Integración HTML y, ind CSS según index.JS y CSS según prototipoprototipo

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,9 @@ const cipher = {
 };
 
 export default cipher;
+// obtener texto desde html = declararlo
+// convertir el texto a carácteres separados tipo Array 
+// convertir a ASCII para determinar valor de cada caracter .charCodeAt()
+// obtener offset desde html = declararlo
+// diseñar función para aplicar formula cipher (x-65+n)%26+65 
+// entregar en HTML texto cifrado .fromCharCode()
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//start button to second page with cipher app
+document.getElementById("start").addEventListener("click", function displayCipher() {
+    const hideLogo = document.getElementById("firstPage");
+    hideLogo.classList.toggle("hidden");
+    const showCipherPage = document.getElementById("cipherPage")
+    showCipherPage.classList.remove("hidden");
+});
+
+//cipher button to show encrypted text
+
+//decipher button to show unencrypted text
+
+//copy button to send resultant text to clipboard
+document.getElementById("copy").addEventListener("click", function copyText() {
+    const copyText = document.getElementById("outputText");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+});
+
+//erase button clears input information
+document.getElementById("erase").addEventListener("click", function clearContents () {
+    document.getElementById("outputText").value = "";
+    document.getElementById("inputText").value = "";
+});
